f218ae310c22ee64f45c368d04e29d7a930021bc
Víctor Manuel <victormanuelap@gmail.com>
2017-03-24 23:17:07 +0100

    Variables de clase en Python: Acceso a una varialbe de clase

---
+++ Presentación del curso y conceptos elementales en Python/004.py
@@ -0,0 +1,9 @@
+class Persona():
+    edad=18
+    def __init__(self,nombre,nacionalidad):
+        self.nombre=nombre
+        self.nacionalidad=nacionalidad
+
+persona1=Persona("Jose","Mexicano")
+
+print(Persona.edad)
