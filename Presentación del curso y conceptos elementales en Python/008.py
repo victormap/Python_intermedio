c53d96a51137f127946ac3d7efadd10190871305
Víctor Manuel <victormanuelap@gmail.com>
2017-03-26 18:18:18 +0200

    Métodos de instancia en Python: Declaración y ejecución de un método de instancia

---
+++ Presentación del curso y conceptos elementales en Python/008.py
@@ -0,0 +1,11 @@
+class Persona():
+    edad=18
+    def __init__(self,nombre,nacionalidad):
+        self.nombre=nombre
+        self.nacionalidad=nacionalidad
+
+    def nadar(self):
+        print("Estoy nadando")
+
+persona1=Persona("Jose","Mexicano")
+persona1.nadar()
