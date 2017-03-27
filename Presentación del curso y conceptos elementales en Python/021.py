0e3d0d08f96611b6c9ca0ee8a4d867fc9df8a657
Víctor Manuel <victormanuelap@gmail.com>
2017-03-27 23:47:38 +0200

    Instrospección en Python: Ejemplo de comprobación de la instrospección a través del comando isinstance

---
+++ Presentación del curso y conceptos elementales en Python/021.py
@@ -0,0 +1,13 @@
+class Intro():
+    def __init__(self,valor):
+        self.valor=valor
+    def segundo(self):
+        print("segundo")
+    def tercero(self):
+        print(tercero)
+
+dato=Intro("valor")
+dir(dato)
+print(dir(dato))
+
+print(isinstance(dato,Intro))
