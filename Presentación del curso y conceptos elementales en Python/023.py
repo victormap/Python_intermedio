6835121ac9d44f202b9a7946eb9b88c6d8644c55
Víctor Manuel <victormanuelap@gmail.com>
2017-03-27 23:52:15 +0200

    Instrospección en Python: Ejemplo de comprobación afirmativa de la instrospección del comando hasattr

---
+++ Presentación del curso y conceptos elementales en Python/023.py
@@ -0,0 +1,16 @@
+class Intro():
+    Introver=9
+    def __init__(self,valor):
+        self.valor=valor
+    def segundo(self):
+        print("segundo")
+    def tercero(self):
+        print(tercero)
+
+dato=Intro("valor")
+dir(dato)
+print(dir(dato))
+
+print(isinstance(dato,Intro))
+
+print(hasattr(dato,"Introver"))
