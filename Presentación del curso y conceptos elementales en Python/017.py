4042ae6ec0e93d978b0c9a542bff7bb8a8312b82
Víctor Manuel <victormanuelap@gmail.com>
2017-03-27 23:11:26 +0200

    Propiedades en Python: Definición y ejecución de una propiedad de clase

---
+++ Presentación del curso y conceptos elementales en Python/017.py
@@ -0,0 +1,11 @@
+class Circulo:
+    
+    def __init__(self,radio):
+        self.radio=radio
+
+    @property
+    def area(self):
+        return 3.1416*(self.radio**2)
+
+c=Circulo(10)
+print(c.area)
