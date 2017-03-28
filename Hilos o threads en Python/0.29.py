c2a1507b6cc28b7b52a37956291f29bd4172b1be
Víctor Manuel <victormanuelap@gmail.com>
2017-03-28 22:29:44 +0200

    Crear un hilo en Python: Creación y ejecución de varios hilos

---
+++ Hilos o threads en Python/0.29.py
@@ -0,0 +1,14 @@
+import threading
+import time
+
+class MiHilo(threading.Thread):
+    def run(self):
+        print("{} inicio".format(self.getName()))
+        time.sleep(1)
+        print("{} terminado".format(self.getName()))
+
+if __name__=="__main__":
+    for x in range(4):
+        hilo=MiHilo(name="Thread-{}".format(x+1))
+        hilo.start()
+        time.sleep(.5)
