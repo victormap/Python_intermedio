e641a1e19a777db9c5509a66d7c3b8b5b0e3882c
Víctor Manuel <victormanuelap@gmail.com>
2017-03-31 11:47:45 +0200

    Ejemplos de programación funcional: Ejemplo de una función de orden superior

---
+++ Programación funcional en Python/040.py
@@ -0,0 +1,12 @@
+def saludo(idioma):
+    def saludo_es():
+        print("Hola")
+    def saludo_en():
+        print("Hi")
+    idioma_func={"es":saludo_es,
+                "en":saludo_en
+                }
+    return idioma_func[idioma]
+
+saludar=saludo("en")
+saludar()
