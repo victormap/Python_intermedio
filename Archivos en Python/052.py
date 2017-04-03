c39040baf9c1fb858d9b6783df11de548a5265fb
Víctor Manuel <victormanuelap@gmail.com>
2017-04-03 12:10:25 +0200

    Leer XML en Python: Leer un archivo XML y mostrar parte de su contenido en pantalla

---
+++ Archivos en Python/052.py
@@ -0,0 +1,7 @@
+from xml.dom.minidom import parse,Node
+xmltree=parse("note.xml")
+
+for nodo in xmltree.getElementsByTagName("pro"):
+    for nodo_hijo in nodo.childNodes:
+        if nodo_hijo.nodeType==Node.TEXT_NODE:
+            print(nodo_hijo.data)
