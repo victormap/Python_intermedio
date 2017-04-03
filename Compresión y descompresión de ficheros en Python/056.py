569123c08df65aee086e34534b62cb20b3a21258
Víctor Manuel <victormanuelap@gmail.com>
2017-04-03 23:28:40 +0200

    Comprimir desde Python en formato Zip: Crear un archivo zip comprimido y mostrar el contenido del mismo en pantalla

---
+++ Compresión y descompresión de ficheros en Python/056.py
@@ -0,0 +1,7 @@
+import zipfile
+from zipfile import ZipFile
+with zipfile.ZipFile('archivo.zip','w') as fzip:
+    fzip.write('Python.docx')
+    fzip.write('Archivo.docx')
+    fzip.write('python1.jpg')
+    fzip.printdir()
